8b696389d79300214c5e9d4eeb5c39a6f70efe3c
Remy Goldschmidt <taktoa@gmail.com>
2017-04-26 11:10:15 -0500

    Added GHCJS.Electron.Utility and dependency on text

---
+++ src/GHCJS/Electron/Utility.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE ExplicitNamespaces #-}
+
+module GHCJS.Electron.Utility
+  ( module GHCJS.Electron.Utility
+  , module Exported
+  ) where
+
+import           Data.Kind    as Exported (type Type)
+import           GHC.TypeLits as Exported (Symbol)

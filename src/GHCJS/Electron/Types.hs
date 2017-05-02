97c516b7b321500219f264375f8e29baaf2e9eed
Remy Goldschmidt <taktoa@gmail.com>
2017-05-02 13:14:18 -0500

    Add documentation stubs to GHCJS.Electron.Types

--- src/GHCJS/Electron/Types.hs
+++ src/GHCJS/Electron/Types.hs
@@ -14,20 +14,25 @@ import           Data.String   (IsString)
 -- | A constructor-less type for use in phantom type arguments.
 data Any
 
+-- FIXME: doc
 newtype Callback (a :: k)
   = MkCallback JSVal
 
+-- FIXME: doc
 newtype JSArray (a :: k)
   = MkJSArray JSVal
 
+-- FIXME: doc
 newtype EventEmitter (a :: k)
   = MkEventEmitter JSVal
 
 --------------------------------------------------------------------------------
 
+-- FIXME: doc
 newtype URL
   = MkURL JSString
 
+-- FIXME: doc
 newtype Path
   = MkPath JSString
 
@@ -38,42 +43,55 @@ newtype Accelerator
   = MkAccelerator JSString
   deriving (IsString)
 
+-- FIXME: doc
 newtype App
   = MkApp JSVal
 
+-- FIXME: doc
 newtype Bookmark
   = MkBookmark JSVal
 
+-- FIXME: doc
 newtype BrowserWindow
   = MkBrowserWindow JSVal
 
+-- FIXME: doc
 newtype BrowserWindowProxy
   = MkBrowserWindowProxy JSVal
 
+-- FIXME: doc
 newtype Clipboard
   = MkClipboard JSVal
 
+-- FIXME: doc
 newtype CommandLine
   = MkCommandLine JSVal
 
+-- FIXME: doc
 newtype Dialog
   = MkDialog JSVal
 
+-- FIXME: doc
 newtype Event
   = MkEvent JSVal
 
+-- FIXME: doc
 newtype GlobalShortcut
   = MkGlobalShortcut JSVal
 
+-- FIXME: doc
 newtype Image
   = MkImage JSVal
 
+-- FIXME: doc
 newtype IPCMain
   = MkIPCMain JSVal
 
+-- FIXME: doc
 newtype Proxy
   = MkProxy JSVal
 
+-- FIXME: doc
 newtype Tray
   = MkTray JSVal
 

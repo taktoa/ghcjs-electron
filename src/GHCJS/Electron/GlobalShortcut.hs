8f161d41bd1d6faf7b26e4be9ce5aa74eb187a4f
Remy Goldschmidt <taktoa@gmail.com>
2017-05-03 10:12:40 -0500

    Cleaned up GHCJS.Electron.GlobalShortcut

--- src/GHCJS/Electron/GlobalShortcut.hs
+++ src/GHCJS/Electron/GlobalShortcut.hs
@@ -1,40 +1,50 @@
 {-# LANGUAGE JavaScriptFFI #-}
 
--- FIXME: doc
-module GHCJS.Electron.GlobalShortcut where
+-- | A wrapper over the Electron global shortcut API, as documented
+--   <https://electron.atom.io/docs/api/global-shortcut here>.
+module GHCJS.Electron.GlobalShortcut
+  ( module Exported
+  , unsafeGetGlobalShortcut
+  , unsafeRegister
+  , unsafeUnregister
+  , unsafeUnregisterAll
+  , unsafeIsRegistered
+  ) where
 
 import           GHCJS.Electron.Types
+
+import           GHCJS.Electron.Types as Exported (GlobalShortcut (..))
 
 -- | Get the canonical 'GlobalShortcut' object, i.e.: the value of
 --   @require('electron').globalShortcut@.
 foreign import javascript safe
   "$r = require('electron').globalShortcut;"
-  getGlobalShortcut :: IO GlobalShortcut
+  unsafeGetGlobalShortcut :: IO GlobalShortcut
 
 -- | Register a callback for the given 'Accelerator'.
 foreign import javascript safe
   "$1.register($2, $3);"
-  register :: GlobalShortcut
-           -> Accelerator
-           -> Callback ()
-           -> IO ()
+  unsafeRegister :: GlobalShortcut
+                 -> Accelerator
+                 -> Callback ()
+                 -> IO ()
 
 -- | Deregister any callbacks registered to the given 'Accelerator'.
 foreign import javascript safe
   "$1.unregister($2);"
-  unregister :: GlobalShortcut
-             -> Accelerator
-             -> IO ()
+  unsafeUnregister :: GlobalShortcut
+                   -> Accelerator
+                   -> IO ()
 
 -- | Deregister all global shortcut callbacks.
 foreign import javascript safe
   "$1.unregisterAll();"
-  unregisterAll :: GlobalShortcut
-                -> IO ()
+  unsafeUnregisterAll :: GlobalShortcut
+                      -> IO ()
 
 -- | Check if the given 'Accelerator' is has been registered.
 foreign import javascript safe
   "$1.isRegistered($2);"
-  isRegistered :: GlobalShortcut
-               -> Accelerator
-               -> IO Bool
+  unsafeIsRegistered :: GlobalShortcut
+                     -> Accelerator
+                     -> IO Bool

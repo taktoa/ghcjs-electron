edc06d291c00e05c827eb74605a10f13dfd2671b
Remy Goldschmidt <taktoa@gmail.com>
2017-05-15 15:54:00 -0500

    Created wrapper module GHCJS.Node.Crypto

---
+++ src/GHCJS/Node/Crypto.hs
@@ -0,0 +1,14 @@
+-- | FIXME: doc
+module GHCJS.Node.Crypto
+  ( module Exported
+  ) where
+
+import           GHCJS.Node.Crypto.Certificate   as Exported
+import           GHCJS.Node.Crypto.Cipher        as Exported
+-- import           GHCJS.Node.Crypto.Decipher      as Exported
+import           GHCJS.Node.Crypto.DiffieHellman as Exported
+import           GHCJS.Node.Crypto.ECDH          as Exported
+import           GHCJS.Node.Crypto.Hash          as Exported
+import           GHCJS.Node.Crypto.HMAC          as Exported
+import           GHCJS.Node.Crypto.Sign          as Exported
+import           GHCJS.Node.Crypto.Verify        as Exported

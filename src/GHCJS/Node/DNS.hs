7ee3b17515f906ac3f1abb5a29e842ab0e043306
Remy Goldschmidt <taktoa@gmail.com>
2017-05-16 14:22:04 -0500

    Added some high-level types to GHCJS.Node.DNS

--- src/GHCJS/Node/DNS.hs
+++ src/GHCJS/Node/DNS.hs
@@ -20,9 +20,154 @@ import           JavaScript.Object
 
 import           GHCJS.Node.Error
 
-import           Data.Text         (Text)
-
 import           Data.Coerce
+
+--------------------------------------------------------------------------------
+
+import           Data.Text         (Text)
+import           Data.Set          (Set)
+
+--------------------------------------------------------------------------------
+
+-- | FIXME: doc
+data DNSRecord
+  = DNSRecordA     !DNSRecordA
+  | DNSRecordAAAA  !DNSRecordAAAA
+  | DNSRecordCNAME !DNSRecordCNAME
+  | DNSRecordMX    !DNSRecordMX
+  | DNSRecordNAPTR !DNSRecordNAPTR
+  | DNSRecordNS    !DNSRecordNS
+  | DNSRecordPTR   !DNSRecordPTR
+  | DNSRecordSOA   !DNSRecordSOA
+  | DNSRecordSRV   !DNSRecordSRV
+  | DNSRecordTXT   !DNSRecordTXT
+
+-- | FIXME: doc
+data DNSRecordType
+  = DNSRecordTypeA
+  | DNSRecordTypeAAAA
+  | DNSRecordTypeCNAME
+  | DNSRecordTypeMX
+  | DNSRecordTypeNAPTR
+  | DNSRecordTypeNS
+  | DNSRecordTypePTR
+  | DNSRecordTypeSOA
+  | DNSRecordTypeSRV
+  | DNSRecordTypeTXT
+
+-- | FIXME: doc
+data DNSRecordA
+  = MkDNSRecordA
+    { _DNSRecordA_address :: !Address
+    , _DNSRecordA_ttl     :: !Int
+    }
+
+-- | FIXME: doc
+data DNSRecordAAAA
+  = MkDNSRecordAAAA
+    { _DNSRecordAAAA_address :: !Address
+    , _DNSRecordAAAA_ttl     :: !Int
+    }
+
+-- | FIXME: doc
+data DNSRecordCNAME
+  = MkDNSRecordCNAME
+    { _DNSRecordCNAME_hostname :: !HostName
+    }
+
+-- | FIXME: doc
+toDNSRecordCNAME :: RecordCNAME -> DNSRecordCNAME
+toDNSRecordCNAME (MkRecordCNAME hostname) = MkDNSRecordCNAME hostname
+
+-- | FIXME: doc
+data DNSRecordMX
+  = MkDNSRecordMX
+    { _DNSRecordMX_priority :: !Int
+    , _DNSRecordMX_exchange :: !HostName
+    }
+
+-- | An NAPTR record flag as defined in
+--   <https://www.ietf.org/rfc/rfc2915.txt IETF RFC 2915>.
+data NAPTRFlag
+  = NAPTRFlagS
+  | NAPTRFlagA
+  | NAPTRFlagU
+  | NAPTRFlagP
+
+-- | A regular expression for NAPTR records as defined in
+--   <https://www.ietf.org/rfc/rfc2915.txt IETF RFC 2915>.
+--
+--   Grammar:
+--     subst_expr   = delim-char  ere  delim-char  repl  delim-char  *flags
+--     delim-char   = "/" / "!" / ... <Any non-digit or non-flag character
+--                    other than backslash '\'. All occurences of a delim_char
+--                    in a subst_expr must be the same character.>
+--     ere          = POSIX Extended Regular Expression
+--     repl         = 1 * ( OCTET /  backref )
+--     backref      = "\" 1POS_DIGIT
+--     flags        = "i"
+--     POS_DIGIT    = %x31-39                 ; 0 is not an allowed backref
+--
+--   FIXME: improve type safety
+type NAPTRRegex = Text
+
+-- | FIXME: doc
+data DNSRecordNAPTR
+  = MkDNSRecordNAPTR
+    { _DNSRecordNAPTR_flags       :: !(Set NAPTRFlag)
+    , _DNSRecordNAPTR_service     :: !Service
+    , _DNSRecordNAPTR_regexp      :: !NAPTRRegex
+    , _DNSRecordNAPTR_replacement :: !Text
+    , _DNSRecordNAPTR_order       :: !Int
+    , _DNSRecordNAPTR_preference  :: !Int
+    }
+
+-- | FIXME: doc
+data DNSRecordNS
+  = MkDNSRecordNS
+    { _DNSRecordNS_hostname :: !HostName
+    }
+
+-- | FIXME: doc
+toDNSRecordNS :: RecordNS -> DNSRecordNS
+toDNSRecordNS (MkRecordNS hostname) = MkDNSRecordNS hostname
+
+-- | FIXME: doc
+data DNSRecordPTR
+  = MkDNSRecordPTR
+    { _DNSRecordPTR_hostname :: !HostName
+    }
+
+-- | FIXME: doc
+toDNSRecordPTR :: RecordPTR -> DNSRecordPTR
+toDNSRecordPTR (MkRecordPTR hostname) = MkDNSRecordPTR hostname
+
+-- | FIXME: doc
+data DNSRecordSOA
+  = MkDNSRecordSOA
+    { _DNSRecordSOA_nsname     :: !HostName
+    , _DNSRecordSOA_hostmaster :: !HostName
+    , _DNSRecordSOA_serial     :: !Int
+    , _DNSRecordSOA_refresh    :: !Int
+    , _DNSRecordSOA_retry      :: !Int
+    , _DNSRecordSOA_expire     :: !Int
+    , _DNSRecordSOA_minttl     :: !Int
+    }
+
+-- | FIXME: doc
+data DNSRecordSRV
+  = MkDNSRecordSRV
+    { _DNSRecordSRV_priority :: !Int
+    , _DNSRecordSRV_weight   :: !Int
+    , _DNSRecordSRV_port     :: !Int
+    , _DNSRecordSRV_name     :: !HostName
+    }
+
+-- | FIXME: doc
+data DNSRecordTXT
+  = MkDNSRecordTXT
+    { _DNSRecordTXT_chunks :: ![Text]
+    }
 
 --------------------------------------------------------------------------------
 
@@ -147,7 +292,7 @@ newtype RecordAAAA
 -- | FIXME: doc
 --   @"bar.example.com"@
 newtype RecordCNAME
-  = MkRecordCNAME JSString
+  = MkRecordCNAME HostName
 
 -- | FIXME: doc
 --   @{priority: 10, exchange: "mx.example.com"}@
@@ -169,12 +314,12 @@ newtype RecordNAPTR
 -- | FIXME: doc
 --   @"ns1.example.com"@
 newtype RecordNS
-  = MkRecordNS JSString
+  = MkRecordNS HostName
 
 -- | FIXME: doc
 --   @"mail.example.com"@
 newtype RecordPTR
-  = MkRecordPTR JSString
+  = MkRecordPTR HostName
 
 -- | FIXME: doc
 --   {
@@ -216,12 +361,15 @@ foreign import javascript safe
 --   A (IPv4) or AAAA (IPv6) record.
 --
 --   This function is equivalent to @dns.lookup(…, {…, all: false}, …)@.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.lookup($1, {family: $2, hints: $3, all: false}, function(e, a, f) { $c(e, a, f); });"
   unsafeLookup :: HostName
                -> Family
                -> Hints
-               -> IO (Nullable Error, Address, Family)
+               -> IO (Nullable Error, Nullable Address, Nullable Family)
 
 -- | Resolves the given hostname (e.g.: @"nodejs.org"@) into the first found
 --   A (IPv4) or AAAA (IPv6) record.
@@ -232,81 +380,120 @@ foreign import javascript interruptible
   unsafeLookupAll :: HostName
                   -> Family
                   -> Hints
-                  -> IO (Nullable Error, Array Resolved)
+                  -> IO (Nullable Error, Nullable (Array Resolved))
 
 -- | Resolves the given address and port into a hostname and service using the
 --   operating system's underlying @getnameinfo@ implementation.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.lookupService($1, $2, function(e, h, s) { $c(e, h, s); });"
   unsafeLookupService :: HostName
                       -> Port
-                      -> IO (Nullable Error, HostName, Service)
+                      -> IO (Nullable Error, Nullable HostName, Nullable Service)
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the A records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolve4($1, {ttl: true}, function(e, a) { $c(e, a); });"
   unsafeResolveA :: HostName
-                 -> IO (Nullable Error, Array RecordA)
+                 -> IO (Nullable Error, Nullable (Array RecordA))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the AAAA records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolve6($1, {ttl: true}, function(e, a) { $c(e, a); });"
   unsafeResolveAAAA :: HostName
-                    -> IO (Nullable Error, Array RecordAAAA)
+                    -> IO (Nullable Error, Nullable (Array RecordAAAA))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the CNAME records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveCname($1, function(e, a) { $c(e, a); });"
   unsafeResolveCNAME :: HostName
-                     -> IO (Nullable Error, Array RecordCNAME)
+                     -> IO (Nullable Error, Nullable (Array RecordCNAME))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the MX records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveMx($1, function(e, a) { $c(e, a); });"
   unsafeResolveMX :: HostName
-                  -> IO (Nullable Error, Array RecordMX)
+                  -> IO (Nullable Error, Nullable (Array RecordMX))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the NAPTR records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveNaptr($1, function(e, a) { $c(e, a); });"
   unsafeResolveNAPTR :: HostName
-                     -> IO (Nullable Error, Array RecordNAPTR)
+                     -> IO (Nullable Error, Nullable (Array RecordNAPTR))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the NS records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveNs($1, function(e, a) { $c(e, a); });"
   unsafeResolveNS :: HostName
-                     -> IO (Nullable Error, Array RecordNS)
+                     -> IO (Nullable Error, Nullable (Array RecordNS))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the PTR records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolvePtr($1, function(e, a) { $c(e, a); });"
   unsafeResolvePTR :: HostName
-                   -> IO (Nullable Error, Array RecordPTR)
+                   -> IO (Nullable Error, Nullable (Array RecordPTR))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the SOA records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveSoa($1, function(e, a) { $c(e, a); });"
   unsafeResolveSOA :: HostName
-                   -> IO (Nullable Error, Array RecordSOA)
+                   -> IO (Nullable Error, Nullable (Array RecordSOA))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the SRV records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveSrv($1, function(e, a) { $c(e, a); });"
   unsafeResolveSRV :: HostName
-                   -> IO (Nullable Error, Array RecordSRV)
+                   -> IO (Nullable Error, Nullable (Array RecordSRV))
 
--- | FIXME: doc
+-- | Uses the DNS protocol to resolve the TXT records for the given hostname.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.resolveTxt($1, function(e, a) { $c(e, a); });"
   unsafeResolveTXT :: HostName
-                   -> IO (Nullable Error, Array RecordTXT)
+                   -> IO (Nullable Error, Nullable (Array RecordTXT))
 
--- | FIXME: doc
+-- | Performs a reverse DNS query that resolves an IPv4 or IPv6 address to an
+--   array of hostnames.
+--
+--   The 'Error' object contains a field named @code@ of type 'DNSError'.
+--
+--   In the returned value, either the 'Error' object is defined or the rest of
+--   the values are.
 foreign import javascript interruptible
   "dns.reverse($1, function(e, h) { $c(e, h); });"
   unsafeReverse :: Address
-                -> IO (Nullable Error, Array HostName)
+                -> IO (Nullable Error, Nullable (Array HostName))
 
 -- | Sets the IP addresses of the servers to be used when resolving.
 --   The @servers@ argument is an array of IPv4 or IPv6 addresses.
